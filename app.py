c99490a7390cfcea2e96e8a92b3967f0cc246beb
kbk <kekekj@naver.com>
2024-10-24 16:52:58 +0900

    making modules2

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, jsonify
-from app import create_app
+from flask import Flask
 
-# app = Flask(__name__)
-app = create_app()
+app = Flask(__name__)
 
+# 라우팅 모듈 등록
+from app.routes import main
+
+app.register_blueprint(main)
 
 if __name__ == "__main__":
     app.run()
