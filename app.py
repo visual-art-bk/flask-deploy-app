f0414b24b8b10659ce3c336d162f2396a420aa37
kbk <kekekj@naver.com>
2024-10-24 16:23:44 +0900

    키워드페이지 추가

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def home():
-    response_data = {
-        'message': 'Hello, Flask!',
-        'status': 'success',
-        'data': {
-            'website': 'gptonline.ai',
-            'year': 2024
-        }
-    }
-    return jsonify(response_data)
+    return """
+    <form action="/crawl" method="post">
+        <label>검색할 키워드: </label>
+        <input type="text" name="keyword">
+        <input type="submit" value="크롤링 시작">
+    </form>
+    """
+
 
 if __name__ == "__main__":
     app.run()
