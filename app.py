ea239825b4dfde4b6753b027009d94b0c15b2daa
kbk <kekekj@naver.com>
2024-10-25 16:32:01 +0900

    test

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from app import create_app
+from flask import Flask
+# from app import create_app
 
-app = create_app()
+# app = create_app()
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "<h1>hello wordl</h1>"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
 
-def create_app():
-    app = Flask(__name__)
+# def create_app():
+#     app = Flask(__name__)
 
-    # 라우팅 모듈을 import하고 등록
-    from app.routes import main
+#     # 라우팅 모듈을 import하고 등록
+#     from app.routes import main
 
-    app.register_blueprint(main)
+#     app.register_blueprint(main)
 
-    return app
+#     return app
