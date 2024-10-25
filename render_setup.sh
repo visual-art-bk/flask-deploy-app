0815aa243a386307e976af3a8b3a00bfb189a74f
kbk <kekekj@naver.com>
2024-10-26 08:46:18 +0900

    Install Chrome

--- render_setup.sh
+++ render_setup.sh
@@ -2,3 +2,9 @@
 
 # Install dependencies
 pip install -r requirements.txt
+
+# Install Chrome
+curl https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update
+sudo apt-get install -y google-chrome-stable
