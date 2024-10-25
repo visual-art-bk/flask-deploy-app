d36fff274cb9cbd1ed08237b545629aaa6ab9e6e
kbk <kekekj@naver.com>
2024-10-26 08:39:59 +0900

    test chrome web driver2

--- app/routes.py
+++ app/routes.py
@@ -4,16 +4,18 @@ from app.WebDriverManager import WebDriverManager
 import time
 # 블루프린트 생성
 main = Blueprint('main', __name__)
-wd = WebDriverManager()
-driver = wd.get_driver()
+
+# wd = WebDriverManager()
+# driver = wd.get_driver()
 
 @main.route('/')
 def home():
     url = 'https://www.naver.com'
     print('Test driver 입니다.')
-    driver.get(url)
-    print(url, "열었습니다.")
-    time.sleep(5)
-    wd.close_driver()
+    
+    # driver.get(url)
+    # print(url, "열었습니다.")
+    # time.sleep(5)
+    # wd.close_driver()
     
     return "<h1>테스트완료</h1>"
--- main.py
+++ main.py
@@ -2,9 +2,5 @@ from app import create_app
 
 app = create_app()
 
-@app.route("/", methods=["GET"])
-def home():
-    return "<h1>Hi Bk 777</h1>"
-
 if __name__ == "__main__":
     app.run(debug=True)
