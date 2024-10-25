08890433ce8b73c1d89184bea6cfd624d05a6f10
kbk <kekekj@naver.com>
2024-10-25 17:43:47 +0900

    test7

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = creat_app()
 
 @app.route("/", methods=["GET"])
 def home():
-    return "<h1>Hi Bk</h1>"
+    return "<h1>Hi Bk 123</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
